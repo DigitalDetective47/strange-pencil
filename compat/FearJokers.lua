df43a206b6b9134f31f93812a13a65b747bfdbfc
DigitalDetective47 <ninji2701@gmail.com>
2025-04-04 23:56:50 -0400

    update mod compats

--- compat/FearJokers.lua
+++ compat/FearJokers.lua
@@ -4,3 +4,11 @@ function SMODS.ObjectTypes.clubs_pack.inject(self)
     self:inject_card(SMODS.Centers.j_tma_Fractal)
     self:inject_card(SMODS.Centers.c_tma_nightfall)
 end
+
+StrangeLib.bulk_add(SMODS.Challenges.c_pencil_immutable.restrictions.banned_cards, {
+    { id = "j_tma_PlagueDoctor" },
+    { id = "j_tma_Wildfire" },
+    { id = "c_tma_nightfall" },
+    { id = "c_tma_burnout" },
+    { id = "c_tma_glimmer" },
+})
