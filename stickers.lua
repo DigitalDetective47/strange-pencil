eaba6154bfb512602e13872508ff31d277e6882f
DigitalDetective47 <ninji2701@gmail.com>
2025-10-25 23:53:43 -0400

    simplify Neon Stake implementation

--- stickers.lua
+++ stickers.lua
@@ -19,7 +19,7 @@ SMODS.Sticker({
     end,
     badge_colour = G.C.YELLOW,
     default_compat = true,
-    should_apply = false,
+    sets = { Joker = true },
     needs_enable_flag = true,
     apply = function(self, card, val)
         if val then
