99eb0657314e93420b0686739b99d7c9645a6490
DigitalDetective47 <ninji2701@gmail.com>
2026-06-26 18:47:39 -0500

    remove inject hook from compat code

--- compat/FearJokers.lua
+++ compat/FearJokers.lua
@@ -1,8 +1,1 @@
-local clubs_pack_inject_hook = SMODS.ObjectTypes.clubs_pack.inject
-function SMODS.ObjectTypes.clubs_pack.inject(self)
-    clubs_pack_inject_hook(self)
-    self:inject_card(SMODS.Centers.j_tma_Fractal)
-    self:inject_card(SMODS.Centers.c_tma_nightfall)
-end
-
 StrangeLib.update_challenge_restrictions("compat/FearJokers/challenge_bans.json")
